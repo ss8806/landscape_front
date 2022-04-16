4d7e489b59b89fd71c61f04015576ba7547a93e9
ss <evo0622@gmail.com>
2022-04-17 00:58:10 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl font-bold">Hello world</h1>
+      <h1 className="text-3xl ">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
