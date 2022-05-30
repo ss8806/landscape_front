4bc75153d41bda918aaff4c79571c137312b60a3
ss <evo0622@gmail.com>
2022-05-30 14:04:11 +0900

    query builder

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,7 +1,17 @@
 import AppLayout from "../../components/Layouts/AppLayout";
-import { useEffect, useState, Suspense, lazy } from "react";
+import {
+  useEffect,
+  useState,
+  Suspense,
+  lazy,
+  Key,
+  ReactChild,
+  ReactFragment,
+  ReactPortal,
+} from "react";
 import axios from "../../lib/axios";
 import { Link } from "react-router-dom";
+import useSWR from "swr";
 
 type Article = {
   id: number;
@@ -15,87 +25,57 @@ type Article = {
 };
 
 const Articles = () => {
-  const Lazy = lazy(() => import("../../components/Lazy"));
   const awspath = "https://backend0622.s3.ap-northeast-1.amazonaws.com/";
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [articles, setArticles] = useState<Article[]>([
-    {
-      id: 0,
-      title: "",
-      body: "",
-      user_id: "",
-      category_id: "",
-      pic1: "",
-      category: "",
-      name: "",
-    },
-  ]);
 
-  useEffect(() => {
-    setLoading(true);
-    setError(false);
+  const fetcher = () =>
     axios
       .get("http://localhost:/api/articles/")
-      .then((response) => {
-        setArticles(response.data);
-        console.log(response.data);
+      .then((res) => {
+        return res.data;
       })
       .catch((error) => {
-        setError(true);
         console.log(error);
-      })
-      .finally(() => {
-        setLoading(false);
       });
-  }, []);
+  const { data, error }: any = useSWR(
+    "http://localhost:/api/articles/",
+    fetcher
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading......</div>;
+  console.log(data);
 
   return (
     <AppLayout>
       <section className="min-h-screen  text-center pb-10  ">
-        {error ? (
-          <p>データを取得できませんでした。</p>
-        ) : loading ? (
-          <Suspense fallback={<p>Loading...</p>}>
-            <Lazy />
-          </Suspense>
-        ) : (
-          <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
-            {articles.map((article) => (
-              <div key={article.id}>
-                <div>
-                  {(article.pic1 && (
-                    <img
-                      id="preview"
-                      src={awspath + article.pic1}
-                      className="d-block mx-auto h-60 h-56"
-                    ></img>
-                  )) || (
-                    <img
-                      id="preview"
-                      className="d-block mx-auto h-60 h-56"
-                      src={`${process.env.PUBLIC_URL}/landscape.svg`}
-                    />
-                  )}
-                </div>
-                <p>タイトル:{article.title}</p>
-                <p>カテゴリー:{article.category.name}</p>
-                {/* {article.cate.map((c_name: { id: any }) => (
-                <div key={c_name.id}>
-                  <p>カテゴリー:{c_name.id}</p>
-                </div>
-              ))} */}
-                <Link
-                  to={"/article/" + article.id + "/show"}
-                  state={{ a_id: article.id }}
-                  className="inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 "
-                >
-                  詳細を見る
-                </Link>
-              </div>
-            ))}
-          </div>
-        )}
+        <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
+          {data.map((article: any) => (
+            <div key={article.article_id}>
+              {(article.pic1 && (
+                <img
+                  id="preview"
+                  src={awspath + article.pic1}
+                  className="d-block mx-auto h-60 h-56"
+                ></img>
+              )) || (
+                <img
+                  id="preview"
+                  className="d-block mx-auto h-60 h-56"
+                  src={`${process.env.PUBLIC_URL}/landscape.svg`}
+                />
+              )}
+              <p>タイトル:{article.title}</p>
+              <p>カテゴリー:{article.c_name}</p>
+              <Link
+                to={"/article/" + article.id + "/show"}
+                state={{ a_id: article.id }}
+                className="inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 "
+              >
+                詳細を見る
+              </Link>
+            </div>
+          ))}
+        </div>
       </section>
     </AppLayout>
   );
