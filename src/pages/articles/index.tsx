cb746a7a49077dc6cfd16e42402e7cb6624c227b
ss <evo0622@gmail.com>
2022-05-31 16:36:04 +0900

    sort 2

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import type { Category } from "../../types/Category";
 import type { Article } from "../../types/Article";
 import useSWR from "swr";
+import moment from "moment";
 import React, { SyntheticEvent, useState, useEffect, useMemo } from "react";
 
 const Articles = () => {
@@ -22,6 +23,48 @@ const Articles = () => {
     "http://localhost:/api/articles/",
     fetcher
   );
+
+  const [filterQuery, setFilterQuery] = useState<any>({});
+  // ソート条件
+  const [sort, setSort] = useState<any>({});
+
+  let [isSorted, setSorted] = useState<boolean>(true);
+
+  const filteredTask = useMemo(() => {
+    let filteredTask = data;
+
+    // 入力した文字は小文字にする
+    const filterTitle = filterQuery.title && filterQuery.title.toLowerCase();
+
+    // 絞り込み検索
+    filteredTask = filteredTask.filter((row: Article) => {
+      // タイトルで絞り込み
+      if (
+        filterQuery.title &&
+        String(row.title).toLowerCase().indexOf(filterTitle) === -1
+      ) {
+        return false;
+      }
+
+      // カテゴリーで絞り込み
+      if (filterQuery.c_id && row.c_id !== parseInt(filterQuery.c_id)) {
+        return false;
+      }
+      return row;
+    });
+
+    // ソート
+    if (sort.key) {
+      filteredTask = filteredTask.sort((a: any, b: any) => {
+        a = a[sort.key];
+        b = b[sort.key];
+        return (a === b ? 0 : a > b ? 1 : -1) * sort.order;
+      });
+    }
+
+    return filteredTask;
+    //第2引数の配列を指定することで、この変数の変化がある度にこの部分の処理が実行されます。
+  }, [filterQuery, sort]);
 
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading......</div>;
@@ -45,13 +88,13 @@ const Articles = () => {
           // onChange={handleFilter}
         >
           <option value="">カテゴリー選択</option>
-          {data[1].map((cate: Category) => {
+          {/* {data[1].map((cate: Category) => {
             return (
               <option key={cate.id} value={cate.id}>
                 {cate.name}
               </option>
             );
-          })}
+          })} */}
         </select>
         <button
           className="w-60 m-4 p-2 bg-white text-base border-solid border border-black"
@@ -60,32 +103,30 @@ const Articles = () => {
           {/* {isSorted ? "登録を古い順に並べ替え" : "登録を新しい順に並べ替え"} */}
         </button>
         <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
-          {data[0].map((article: Article) => (
-            <div key={article.article_id}>
-              {(article.pic1 && (
-                <img
-                  id="preview"
-                  src={awspath + article.pic1}
-                  className="d-block mx-auto h-60 h-56"
-                ></img>
-              )) || (
-                <img
-                  id="preview"
-                  className="d-block mx-auto h-60 h-56"
-                  src={`${process.env.PUBLIC_URL}/landscape.svg`}
-                />
-              )}
-              <p>タイトル:{article.title}</p>
-              <p>カテゴリー:{article.c_name}</p>
-              <Link
-                to={"/article/" + article.article_id + "/show"}
-                state={{ a_id: article.article_id }}
-                className="inline-flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2 "
-              >
-                詳細を見る
-              </Link>
-            </div>
-          ))}
+          {filteredTask.length ? (
+            filteredTask.map((article: Article) => {
+              return (
+                <div key={article.id} className="">
+                  <img
+                    className="g:h-60 xl:h-56 md:h-64 sm:h-72 xs:h-72 h-72 rounded w-full object-cover object-center mb-6"
+                    src="https://i.imgur.com/lmYYa2s.png"
+                  />
+                  <div className="text-center">
+                    カテゴリー：
+                    {article.c_name[0].name}
+                  </div>
+                  <div className="text-center">{article.title}</div>
+                  {/* <div className="text-center">
+                                            {moment(article.create).format(
+                                                "YYYY年MM月DD日"
+                                            )}
+                                        </div> */}
+                </div>
+              );
+            })
+          ) : (
+            <p>該当なし</p>
+          )}
         </div>
       </section>
     </AppLayout>
